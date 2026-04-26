d453fd9ba4b4f6ca3f625b99a435607601a867b1
damiennikon <damiennikon@gmail.com>
2026-04-26 16:17:11 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,52 @@
-const CACHE_NAME = 'dso-tracker-v1';
+const CACHE_NAME = 'dso-tracker-v2'; // Upgraded to v2!
 
-// This is the list of files the phone will download to work offline
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json',
-    '/Galaxy.png',
-    '/Nebula.png',
-    '/Cluster.png',
-    '/icon-192.png',
-    '/icon-512.png'
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/Galaxy.png',
+  '/Nebula.png',
+  '/Cluster.png',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Install Event - Caches the files
+// Install Event - Caches the files and forces immediate takeover
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(ASSETS_TO_CACHE);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+  self.skipWaiting(); 
+});
+
+// Activate Event - Wipes out the old Zombie Cache (v1)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 // Fetch Event - Serves the files from cache if offline
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
